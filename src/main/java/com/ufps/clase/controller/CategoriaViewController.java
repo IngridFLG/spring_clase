1ccd37eefbb8de2794fe5e7cf4806a0ee649baf6
Ingrid Florez <ingrid.florez0226@gmail.com>
2023-05-08 11:30:03 -0500

    endpoint para insertar categorias con la vista en thymeleaf

--- src/main/java/com/ufps/clase/controller/CategoriaViewController.java
+++ src/main/java/com/ufps/clase/controller/CategoriaViewController.java
@@ -5,11 +5,16 @@ import java.util.List;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 import com.ufps.clase.entities.Categoria;
 import com.ufps.clase.repository.CategoriaRepository;
+
+import jakarta.validation.Valid;
 
 @Controller
 @RequestMapping("/categorias_view")
@@ -25,5 +30,36 @@ public class CategoriaViewController {
 		model.addAttribute("titulo2", "Listado de categorias");
 		return "index";
 	}
+	
+    @GetMapping("/registrar")
+    public String mostrarFormulario(Categoria categoria) {
+        return "registrar";
+    }
+
+    @PostMapping("/registrar")
+    public String procesarFormulario(@Valid Categoria categoria, BindingResult result, Model model) {
+        if (result.hasErrors()) {
+            return "registrar";
+        }
+
+        Categoria categoriaGuardada = categoriaRepository.save(categoria);
+        model.addAttribute("categoria", categoriaGuardada);
+
+        return "redirect:/categorias_view/listar";
+    }
+    
+    @PostMapping("/editar")
+    public String editarCategoria(@ModelAttribute("categoria") @Valid Categoria categoria,
+                                  BindingResult bindingResult,
+                                  Model model) {
+        if (bindingResult.hasErrors()) {
+            return "registrar";
+        }
+        // Lógica para actualizar la categoría en la base de datos
+        model.addAttribute("mensaje", "La categoría ha sido actualizada exitosamente.");
+        return "redirect:/categorias_view/listar";
+    }
 
 }
+
+
